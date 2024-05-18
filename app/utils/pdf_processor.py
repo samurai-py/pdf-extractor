786122126841e74ab09e271d1269d0ee841b2025
samurai-py <marioestevao@gmail.com>
2024-05-18 01:05:14 -0300

    "adjusting pdf_processor.py"

--- app/utils/pdf_processor.py
+++ app/utils/pdf_processor.py
@@ -1,23 +1,26 @@
 import pdfplumber
 import re
+import io
 
 def extract_pdf_data(pdf_bytes):
     with pdfplumber.open(io.BytesIO(pdf_bytes)) as pdf:
+        # Selecionando a primeira página do PDF
         page = pdf.pages[0]
         text = page.extract_text()
 
         # Extraindo informações específicas do texto
+        lines = text.split('\n')
         customer_data = {
-            "name": extract_value(text, r'Customer Name\n(.*?)\n'),
-            "street": extract_value(text, r'Street\n(.*?)\n'),
-            "postcode": extract_value(text, r'Postcode City\n(.*?)\n'),
-            "country": extract_value(text, r'Country\n(.*?)\n')
+            "name": ' '.join(lines[6].split()[:2]),
+            "street": lines[7].strip(),
+            "postcode": lines[8].strip(),
+            "country": lines[9].strip()
         }
 
         invoice_details = {
-            "date": extract_value(text, r'Invoice date: (.*?)\n'),
-            "id": extract_value(text, r'Invoice number: (.*?)\n'),
-            "payment_due": extract_value(text, r'Payment due: (.*?)\n'),
+            "date": extract_value(text, r'Invoice date:\s*(.*?)\n'),
+            "id": extract_value(text, r'Invoice number:\s*(.*?)\n'),
+            "payment_due": extract_value(text, r'Payment due:\s*(.*?)\n'),
             "payments": [
                 {
                     "description": extract_value(text, r'Description\n(.*?)\n'),
@@ -29,11 +32,11 @@ def extract_pdf_data(pdf_bytes):
         }
 
         recipient_bank_details = {
-            "bank_account_name": extract_value(text, r'Bank account name: (.*?)\n'),
-            "bank_name": extract_value(text, r'Name of Bank: (.*?)\n'),
-            "account_number": extract_value(text, r'Bank account number: (.*?)\n'),
-            "swift_code": extract_value(text, r'Bank SWIFT code: (.*?)\n'),
-            "bank_address": extract_value(text, r'Bank address: (.*?)\n')
+            "bank_account_name": extract_value(text, r'Bank account name:*(.*?)\n'),
+            "bank_name": extract_value(text, r'Name of Bank:*(.*?)\n'),
+            "account_number": extract_value(text, r'Bank account number:*(.*?)\n'),
+            "swift_code": extract_value(text, r'Bank SWIFT code:*(.*?)\n'),
+            "bank_address": extract_value(text, r'Bank address:*(.*?)\n')
         }
 
         return {
